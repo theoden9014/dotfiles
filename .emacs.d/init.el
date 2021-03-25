1d6dbd4a08fb7deadfa6dbc6338a70e4efbb1938
Junki Kaneko <junki.kaneko@dena.jp>
2021-03-25 21:43:37 +0900

    remove needless config in Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,31 +1,11 @@
 
-(require 'cl)
+;; (require 'cl)
 
 ;;--------------------------------------------------------------------------------
 ;; Load Path
 ;;--------------------------------------------------------------------------------
 (defvar darwin-p (eq system-type 'darwin))      ; for Mac OS X
 (defvar nt-p (eq system-type 'windows-nt))      ; for Windows
-
-;; This is load configuration for mode.
-;; Load from Path variable.
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-              (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-loadpath)
-            (normal-top-level-add-subdirs-to-load-path))))))
-
-;; Set preference directory
-(setq preferences-directory "~/.emacs.d/")
-
-;; Branch OS
-(add-to-load-path "config")
-(if (eq system-type 'gnu/linux) (load "linux"))  ; Linux
-(if (eq system-type 'windows-nt) (load "windows")) ; Windows
-(if (eq system-type 'mac) (load "mac")) ; Mac
 
 ;; for GUI (window-system)
 (when window-system (load "window-system"))
@@ -34,68 +14,31 @@
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
 
-;; Load Configuration File for each Emacs Version
-(defun load-file-in-dir (dir file)
-  (load (concat dir file)))
-(put 'narrow-to-region 'disabled nil)
-
-
 ;;--------------------------------------------------------------------------------
 ;; Package Manager
 ;;--------------------------------------------------------------------------------
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq package-archives
+        '(("marmalade"   . "http://marmalade-repo.org/packages/")
+          ("gnu"         . "http://elpa.gnu.org/packages/")
+          ("org"         . "http://orgmode.org/elpa/")
+          ("melpa"       . "https://melpa.org/packages/")
+          ("melpa-stable" . "https://stable.melpa.org/packages/")))
+;; disable automatic loading of packages after the init file
+(setq package-enable-at-startup nil)
+;; instead load them explicitly
 (package-initialize)
+;; refresh package descriptions
+(unless package-archive-contents
+   (package-refresh-contents))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; check if each package is installed and install it if is not
-(dolist (pkg '(use-package flycheck))
-  (unless (package-installed-p pkg)
-    (package-refresh-contents)
-    (package-install pkg)
-    ))
-
-(eval-when-compile
-   (require 'use-package))
-
-
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
-
-
-(use-package flycheck)
-
-(defvar installing-package-list
-  '(
-    json-mode
-    auto-complete
-    yaml-mode
-    terraform-mode
-    web-mode
-    haml-mode
-    dockerfile-mode
-    go-mode
-    lsp-mode
-
-    use-package
-    dap-mode
-    which-key
-
-    flycheck
-    ))
-
-(let ((not-installed (loop for x in installing-package-list
-                           when (not (package-installed-p x))
-                           collect x)))
-  (when not-installed
-    (package-refresh-contents)
-    (dolist (pkg not-installed)
-      (package-install pkg))))
+;;; use-package initialization
+;;; install use-package if not already done
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+;;; use-package for all others
+(require 'use-package)
 
 
 ;;--------------------------------------------------------------------------------
@@ -132,212 +75,62 @@
 
 
 ;;--------------------------------------------------------------------------------
-;; Package configuration
-;;--------------------------------------------------------------------------------
-;; auto-complete
-(require 'auto-complete)
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20130724.1750/dict")
-(require 'auto-complete-config)
-(ac-config-default)
-(global-auto-complete-mode t)
-
-
-;;--------------------------------------------------------------------------------
 ;; Language configuration
 ;;--------------------------------------------------------------------------------
-;; c++-mode
-(defun add-c++-mode-conf ()
-  (c-set-style "stroustrup")
-  (setq indent-tabs-mode nil)
-  (c-set-offset 'innamespace 0)   ;; don't indent for "namespace {}"
-  (c-set-offset 'arglist-close 0) ;; don't indent for closing parenthesis of the argument list of the function
-  (local-set-key (kbd "RET") 'newline-and-indent)
-  )
-(add-hook 'c++-mode-hook 'add-c++-mode-conf)
-
-;; c-mode
-(defun add-c-mode-conf ()
-  (c-set-style "k&r")
-  (local-set-key (kbd "RET") 'newline-and-indent)
-  )
-(add-hook 'c-mode-hook 'add-c-mode-conf)
-
-;; diff mode
-;; ;; Change Display
-;; (defun diff-mode-setup-faces ()
-;;   ;; Show green if add rows
-;;   (set-face-attribute 'diff-added nil
-;;                    :foreground "white" :background "dark green")
-;;   ;; Show red if delete rows
-;;   (set-face-attribute 'diff-removed nil
-;;                    :foreground "white" :background "dark red"))
-;; (add-hook-fn 'diff-mode-hook (diff-mode-setup-faces))
-(defun add-diff-mode-conf ()
-  ;; Show green if add rows
-  (set-face-attribute 'diff-added nil
-                                            :foreground "white" :background "dark green")
-  ;; Show red if delete rows
-  (set-face-attribute 'diff-removed nil
-                                            :foreground "white" :background "dark red")
-  )
-(add-hook 'diff-mode-hokk 'add-diff-mode-conf)
-
-;; lsp-mode
+;; Go
 (use-package lsp-mode
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  :init (setq lsp-keymap-prefix "s-l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (go-mode . lsp)
-	 (go-mode . lsp-deferred)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration)
-	 )
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
   :config
-  (setq lsp-enable-file-watchers t)
-  (setq lsp-file-watch-threshold 5000)
-  :commands (lsp lsp-deferred))
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-;; if you are helm user
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-(use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; optional if you want which-key integration
-(use-package which-key
-  :config
-  (which-key-mode))
-
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
 ;; Optional - provides snippet support.
 (use-package yasnippet
   :ensure t
   :commands yas-minor-mode
   :hook (go-mode . yas-minor-mode))
 
-;; go-mode
-(use-package go-mode
-  :commands go-mode
-  :mode (("\\.go?\\'" . go-mode))
-  :defer t
-  :config
-  (setq indent-tabs-mode nil)
-  (setq c-basic-offset 4)
-  (setq tab-width 4)
-  (add-hook 'before-save-hook 'lsp-format-buffer)
-  (add-hook 'before-save-hook 'lsp-organize-imports))
 
-;; json-mode
-(defun add-json-mode-conf ()
-  (setq json-reformat:indent-width 2)
-  (setq js-indent-level 2)
-  (setq json-reformat:pretty-string? nil))
-
-(add-hook 'json-mode-hook 'add-json-mode-conf)
-
-;; org-mode
-(unless package-archive-contents    ;; Refresh the packages descriptions
-  (package-refresh-contents))
-(setq package-load-list '(all))     ;; List of packages to load
-(unless (package-installed-p 'org)  ;; Make sure the Org package is
-  (package-install 'org))           ;; installed, install it if not
-(package-initialize)                ;; Initialize & Install Package
-;; (setq org-...)                   ;; Your custom settings
-
-
-;; perl-mode
-(defun add-perl-mode-conf ()
-  (c-set-offset 'case-label 4)
-  (c-set-offset 'arglist-intro 4)
-  (c-set-offset 'arglist-cont-nonempty 4)
-  (c-set-offset 'arglist-close 0)
-  (setq c-basic-offset 4     ;; basic indent value
-        tab-width 4          ;; tab value
-        indent-tabs-mode nil)'
-  (c-toggle-auto-hungry-state 1)     ;; if input ';' , auto create a new line and indent
-  ;;      (define-key c-mode-base-map "\C-m" 'newline-and-indent)    ;; if input "Return" key, auto create anew line and indent
-  (local-set-key (kbd "RET") 'newline-and-indent)
+;; Ruby
+(use-package ruby-mode
+  :ensure t
+  :commands ruby-mode
   )
-(add-hook 'perl-mode-hook 'add-perl-mode-conf)
-
-;; php-mode
-(add-hook 'php-mode-hook
-	  (lambda ()
-	    (c-set-offset 'case-label 4)
-	    (c-set-offset 'arglist-intro 4)
-	    (c-set-offset 'arglist-cont-nonempty 4)
-	    (c-set-offset 'arglist-close 0)
-	    (setq c-basic-offset 4 ;; basic indent value
-		  tab-width 4 ;; tab value
-		  indent-tabs-mode nil)'
-	    (c-toggle-auto-hungry-state 1) ;; if input ';' , auto create a new line and indent
-	    (define-key c-mode-base-map "\C-m" 'newline-and-indent) ;; if input "Return" key, auto create anew line and indent
-	    (local-set-key (kbd "RET") 'newline-and-indent)))
-
-
-;; ruby-mode
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-
-(defun add-ruby-mode-conf ()
-  (setq tab-width 2)
-  (setq ruby-indent-level tab-width)
-  (setq ruby-deep-indent-paren-style nil)
-;  (define-key ruby-mode-map [return] 'ruby-reindent-then-newline-and-indent)))
-  (define-key ruby-mode-map [return] 'reindent-then-newline-and-indent)
-  )
-(add-hook 'ruby-mode-hook 'add-ruby-mode-conf)
-
-;; web-mode
-(add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
-(setq web-mode-content-types-alist
-      '(("jsx" . "\\.js[x]?\\'")))
-
-;; indent
-(add-hook 'web-mode-hook
-          '(lambda ()
-             (setq web-mode-attr-indent-offset nil)
-             (setq web-mode-markup-indent-offset 2)
-             (setq web-mode-css-indent-offset 2)
-             (setq web-mode-code-indent-offset 2)
-             (setq web-mode-sql-indent-offset 2)
-             (setq indent-tabs-mode nil)
-             (setq tab-width 2)
-          ))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-comment-face ((t (:foreground "#587F35"))))
- '(web-mode-css-at-rule-face ((t (:foreground "#DFCF44"))))
- '(web-mode-css-property-name-face ((t (:foreground "#87CEEB"))))
- '(web-mode-css-pseudo-class ((t (:foreground "#DFCF44"))))
- '(web-mode-css-selector-face ((t (:foreground "#DFCF44"))))
- '(web-mode-css-string-face ((t (:foreground "#D78181"))))
- '(web-mode-doctype-face ((t (:foreground "#4A8ACA"))))
- '(web-mode-html-attr-equal-face ((t (:foreground "#FFFFFF"))))
- '(web-mode-html-attr-name-face ((t (:foreground "#87CEEB"))))
- '(web-mode-html-attr-value-face ((t (:foreground "#D78181"))))
- '(web-mode-html-tag-face ((t (:foreground "#4A8ACA"))))
- '(web-mode-server-comment-face ((t (:foreground "#587F35")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (which-key dap-mode yasnippet yaml-mode web-mode use-package terraform-mode lsp-ui json-mode haml-mode go-mode flycheck dockerfile-mode company-lsp auto-complete))))
+   '(dockerfile-mode yaml-mode protobuf-mode ruby-mode yasnippet use-package org lsp-ui company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Protocol Buffers
+(use-package protobuf-mode
+  :ensure t
+  )
+
+;; YAML
+(use-package yaml-mode
+  :ensure t
+  )
